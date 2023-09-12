341ccb3c06914b013a54600c8c870486a22285a6
Orlova-Svetlana74 <sorlova#mail.ru>
2023-09-13 01:19:30 +0300

    fixuseNavigate

--- src/page/routers.jsx
+++ src/page/routers.jsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Entrance from '../components/entrance/entrance';
-// import { ProtectedRoute } from '../components/protected-routs/protected-routs';
-// import MessageGeter from './components/messagegeter';
-// import MessageSender from '../components/messagesender';
+import { ProtectedRoute } from '../components/protected-routs/protected-routs';
+import MessageGeter from './components/messagegeter';
+import MessageSender from '../components/messagesender';
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/entrance" element={<Entrance />} />
-      {/* <Route element={<ProtectedRoute />}>
+      <Route element={<ProtectedRoute />}>
         <Route path="/" element={<MessageGeter />} />
         <Route path="/" element={<MessageSender />} />
-      </Route> */}
+      </Route>
     </Routes>
   );
 }
